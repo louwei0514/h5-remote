278de8ef3f6bfc4c205a343d5ba6adcd11a7808c
louwei0514 <1731830189@qq.com>
2026-07-10 15:27:05 +0800

    11

--- android/app/src/main/java/com/tv/remote/MainActivity.java
+++ android/app/src/main/java/com/tv/remote/MainActivity.java
@@ -1,5 +1,19 @@
 package com.tv.remote;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // 允许 WebView 从 capacitor:// 安全上下文发起到 ws:// 局域网的非加密 WebSocket 连接
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+}
